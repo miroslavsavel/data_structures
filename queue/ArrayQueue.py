a5d512e88a9f2e1b785c101cb7f34c1bce4335db
Miroslav Savel <miroslav.savel@gmail.com>
2022-08-16 15:48:45 +0200

    queue - first and dequeue

--- queue/ArrayQueue.py
+++ queue/ArrayQueue.py
@@ -1,3 +1,5 @@
+from Empty import Empty
+
 class ArrayQueue:
     """FIFO queue implementation using a python list as underlying storage"""
     DEFAULT_CAPACITY = 10       # moderate capacity for all new queues
@@ -15,3 +17,20 @@ class ArrayQueue:
     def is_empty(self):
         """Return True if the queue is empty"""
         return self._size == 0
+
+    def first(self):
+        """Return (but do not remove) the element at the front of the queue
+        If empty - raise exception
+        """
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        return self._data[self._front]
+
+    def dequeue(self):
+        """Remove and return the firist element of the queue FIFO"""
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        answer = self._data[self._front]                # help garbage collection
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
